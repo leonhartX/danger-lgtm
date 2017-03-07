0e64569d712ef45728e914d4ca04759d274ff692
Ke Xu <leonhartx.k@gmail.com>
2017-03-07 17:34:13 +0900

    first commit

--- lib/lgtm/plugin.rb
+++ lib/lgtm/plugin.rb
@@ -17,17 +17,9 @@ module Danger
   # @tags monday, weekends, time, rattata
   #
   class DangerLgtm < Plugin
-
-    # An attribute that you can read/write from your Dangerfile
-    #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
-
-    # A method that you can call from your Dangerfile
-    # @return   [Array<String>]
-    #
-    def warn_on_mondays
-      warn 'Trying to merge code on a Monday' if Date.today.wday == 1
+    def lgtm
+      puts status_report
+      message('lgtm!');
     end
   end
 end
--- spec/lgtm_spec.rb
+++ spec/lgtm_spec.rb
@@ -12,30 +12,17 @@ module Danger
     describe 'with Dangerfile' do
       before do
         @dangerfile = testing_dangerfile
-        @my_plugin = @dangerfile.lgtm
+        @lgtm = @dangerfile.lgtm
       end
 
       # Some examples for writing tests
       # You should replace these with your own.
 
-      it "Warns on a monday" do
-        monday_date = Date.parse("2016-07-11")
-        allow(Date).to receive(:today).and_return monday_date
+      it "lgtm with no violation" do
+        @lgtm.lgtm
 
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq(["Trying to merge code on a Monday"])
+        expect(@dangerfile.status_report[:message].length).to eq(1)
       end
-
-      it "Does nothing on a tuesday" do
-        monday_date = Date.parse("2016-07-12")
-        allow(Date).to receive(:today).and_return monday_date
-
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq([])
-      end
-
     end
   end
 end
